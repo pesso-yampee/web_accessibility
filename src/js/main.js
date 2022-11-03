8860f5527a35cf983ab27a03d7556900184817da
yamasaki junpei <opss0622@gmail.com>
2022-11-03 12:03:59 +0900

    ハンバーガーメニューの挙動を調整

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,44 @@
-"use strict";
-
 window.addEventListener("DOMContentLoaded", () => {
+  const openMenuAnimate = [
+    {
+      visibility: "hidden",
+      opacity: 0,
+      display: "none",
+    },
+    {
+      visibility: "hidden",
+      opacity: 0,
+      display: "block",
+    },
+    {
+      visibility: "visible",
+      opacity: 1,
+      display: "block",
+    },
+  ];
+  const openMenuAnimateDuration = {
+    duration: 500,
+  };
+  const closeMenuAnimate = [
+    {
+      visibility: "visible",
+      opacity: 1,
+    },
+    {
+      visibility: "hidden",
+      opacity: 0,
+    },
+    {
+      visibility: "hidden",
+      opacity: 0,
+    },
+  ];
+  const closeMenuAnimateDuration = {
+    duration: 500,
+  };
   // メニュー展開時に背景を固定
   const backgroundFix = (bool) => {
     const scrollingElement = () => {
-      const browser = window.navigator.userAgent.toLowerCase();
       if ("scrollingElement" in document) return document.scrollingElement;
       return document.documentElement;
     };
@@ -35,33 +69,42 @@ window.addEventListener("DOMContentLoaded", () => {
     const e = props.event;
     const currentTarget = e.currentTarget;
     const menu = props.menu;
-    let flag = props.flg;
+    let flg = props.flg;
 
     //ハンバーガーボタンが選択されたら
     currentTarget.classList.toggle(CLASS);
     menu.classList.toggle(CLASS);
-    if (flag) {
+    if (flg) {
       // flagの状態で制御内容を切り替え
+      menu.animate(closeMenuAnimate, closeMenuAnimateDuration);
+      setTimeout(() => {
+        menu.style.display = "none";
+      }, 500);
       backgroundFix(false);
       currentTarget.setAttribute("aria-expanded", "false");
       menu.setAttribute("aria-hidden", "true");
       currentTarget.focus();
-      flag = false;
+      flg = false;
     } else {
+      menu.style.display = "";
+      menu.animate(openMenuAnimate, openMenuAnimateDuration);
       backgroundFix(true);
       menu.setAttribute("aria-hidden", "false");
       currentTarget.setAttribute("aria-expanded", "true");
-      flag = true;
+      flg = true;
     }
+
+    return flg;
   };
 
   //escキー押下でメニューを閉じられるように
   const closeMenuByEscapeKey = (props) => {
+    const e = props.e;
     const CLASS = props.class;
     const humberger = props.humberger;
-    const flag = props.flag;
+    let flag = props.flag;
 
-    if (event.key === "Escape") {
+    if (e.key === "Escape") {
       humberger.classList.remove(CLASS);
       menu.classList.remove(CLASS);
 
@@ -70,6 +113,8 @@ window.addEventListener("DOMContentLoaded", () => {
       humberger.setAttribute("aria-expanded", "false");
       flg = false;
     }
+
+    return flag;
   };
 
   // メニュー内アコーディオン制御
@@ -87,36 +132,38 @@ window.addEventListener("DOMContentLoaded", () => {
       e.currentTarget.setAttribute("aria-expanded", "true");
       accordionFlg = true;
     }
+
+    return accordionFlg;
   };
 
   // 変数定義
   const CLASS = "-active";
-  let flg = false;
-  let accordionFlg = false;
-
   const humberger = document.getElementById("js-humberger");
   const focusTrap = document.getElementById("js-focusTrap");
   const menu = document.querySelector(".js-nav");
   const accordionTrigger = document.querySelectorAll(".js-accordionTrigger");
+  let flg = false;
+  let accordionFlg = false;
 
   humberger.addEventListener("click", (e) => {
     const props = {
       event: e,
       menu: menu,
-      flag: flg,
+      flg: flg,
     };
 
-    toggleShowMenu(props);
+    flg = toggleShowMenu(props);
   });
 
-  window.addEventListener("keydown", () => {
+  window.addEventListener("keydown", (e) => {
     const props = {
+      e: e,
       class: CLASS,
       humberger: humberger,
       flag: flg,
     };
 
-    closeMenuByEscapeKey(props);
+    flg = closeMenuByEscapeKey(props);
   });
 
   accordionTrigger.forEach((item) => {
@@ -127,7 +174,7 @@ window.addEventListener("DOMContentLoaded", () => {
         accordionFlg: accordionFlg,
       };
 
-      toggleShowAccordionInMenu(props);
+      accordionFlg = toggleShowAccordionInMenu(props);
     });
   });
 
