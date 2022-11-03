b7da36bbbfafb11ac1cd53555d566cb7388b5404
yamasaki junpei <opss0622@gmail.com>
2022-11-03 10:18:25 +0900

    JSリファクタリング

--- src/js/main.js
+++ src/js/main.js
@@ -1,80 +1,83 @@
 "use strict";
 
-// メニュー展開時に背景を固定
-const backgroundFix = (bool) => {
-  const scrollingElement = () => {
-    const browser = window.navigator.userAgent.toLowerCase();
-    if ("scrollingElement" in document) return document.scrollingElement;
-    return document.documentElement;
+window.addEventListener("DOMContentLoaded", () => {
+  // メニュー展開時に背景を固定
+  const backgroundFix = (bool) => {
+    const scrollingElement = () => {
+      const browser = window.navigator.userAgent.toLowerCase();
+      if ("scrollingElement" in document) return document.scrollingElement;
+      return document.documentElement;
+    };
+
+    const scrollY = bool
+      ? scrollingElement().scrollTop
+      : parseInt(document.body.style.top || "0");
+
+    const fixedStyles = {
+      height: "100vh",
+      position: "fixed",
+      top: `${scrollY * -1}px`,
+      left: "0",
+      width: "100vw",
+    };
+
+    Object.keys(fixedStyles).forEach((key) => {
+      document.body.style[key] = bool ? fixedStyles[key] : "";
+    });
+
+    if (!bool) {
+      window.scrollTo(0, scrollY * -1);
+    }
   };
 
-  const scrollY = bool
-    ? scrollingElement().scrollTop
-    : parseInt(document.body.style.top || "0");
+  // メニュー開閉制御
+  const toggleShowMenu = (props) => {
+    const e = props.event;
+    const currentTarget = e.currentTarget;
+    const menu = props.menu;
+    let flag = props.flg;
 
-  const fixedStyles = {
-    height: "100vh",
-    position: "fixed",
-    top: `${scrollY * -1}px`,
-    left: "0",
-    width: "100vw",
+    //ハンバーガーボタンが選択されたら
+    currentTarget.classList.toggle(CLASS);
+    menu.classList.toggle(CLASS);
+    if (flag) {
+      // flagの状態で制御内容を切り替え
+      backgroundFix(false);
+      currentTarget.setAttribute("aria-expanded", "false");
+      menu.setAttribute("aria-hidden", "true");
+      currentTarget.focus();
+      flag = false;
+    } else {
+      backgroundFix(true);
+      menu.setAttribute("aria-hidden", "false");
+      currentTarget.setAttribute("aria-expanded", "true");
+      flag = true;
+    }
   };
 
-  Object.keys(fixedStyles).forEach((key) => {
-    document.body.style[key] = bool ? fixedStyles[key] : "";
-  });
-
-  if (!bool) {
-    window.scrollTo(0, scrollY * -1);
-  }
-};
-
-// 変数定義
-const CLASS = "-active";
-let flg = false;
-let accordionFlg = false;
-
-let humberger = document.getElementById("js-humberger");
-let focusTrap = document.getElementById("js-focus-trap");
-let menu = document.querySelector(".js-nav-area");
-let accordionTrigger = document.querySelectorAll(".js-sp-accordion-trigger");
-let accordion = document.querySelectorAll(".js-sp-accordion");
-
-// メニュー開閉制御
-humberger.addEventListener("click", (e) => {
-  //ハンバーガーボタンが選択されたら
-  e.currentTarget.classList.toggle(CLASS);
-  menu.classList.toggle(CLASS);
-  if (flg) {
-    // flgの状態で制御内容を切り替え
-    backgroundFix(false);
-    humberger.setAttribute("aria-expanded", "false");
-    menu.setAttribute("aria-hidden", "true");
-    humberger.focus();
-    flg = false;
-  } else {
-    backgroundFix(true);
-    menu.setAttribute("aria-hidden", "false");
-    humberger.setAttribute("aria-expanded", "true");
-    flg = true;
-  }
-});
-window.addEventListener("keydown", () => {
   //escキー押下でメニューを閉じられるように
-  if (event.key === "Escape") {
-    humberger.classList.remove(CLASS);
-    menu.classList.remove(CLASS);
+  const closeMenuByEscapeKey = (props) => {
+    const CLASS = props.class;
+    const humberger = props.humberger;
+    const flag = props.flag;
 
-    backgroundFix(false);
-    humberger.focus();
-    humberger.setAttribute("aria-expanded", "false");
-    flg = false;
-  }
-});
+    if (event.key === "Escape") {
+      humberger.classList.remove(CLASS);
+      menu.classList.remove(CLASS);
 
-// メニュー内アコーディオン制御
-accordionTrigger.forEach((item) => {
-  item.addEventListener("click", (e) => {
+      backgroundFix(false);
+      humberger.focus();
+      humberger.setAttribute("aria-expanded", "false");
+      flg = false;
+    }
+  };
+
+  // メニュー内アコーディオン制御
+  const toggleShowAccordionInMenu = (props) => {
+    const e = props.event;
+    const CLASS = props.CLASS;
+    let accordionFlg = props.accordionFlg;
+
     e.currentTarget.classList.toggle(CLASS);
     e.currentTarget.nextElementSibling.classList.toggle(CLASS);
     if (accordionFlg) {
@@ -84,10 +87,52 @@ accordionTrigger.forEach((item) => {
       e.currentTarget.setAttribute("aria-expanded", "true");
       accordionFlg = true;
     }
-  });
-});
+  };
 
-// フォーカストラップ制御
-focusTrap.addEventListener("focus", (e) => {
-  humberger.focus();
+  // 変数定義
+  const CLASS = "-active";
+  let flg = false;
+  let accordionFlg = false;
+
+  const humberger = document.getElementById("js-humberger");
+  const focusTrap = document.getElementById("js-focusTrap");
+  const menu = document.querySelector(".js-nav");
+  const accordionTrigger = document.querySelectorAll(".js-accordionTrigger");
+
+  humberger.addEventListener("click", (e) => {
+    const props = {
+      event: e,
+      menu: menu,
+      flag: flg,
+    };
+
+    toggleShowMenu(props);
+  });
+
+  window.addEventListener("keydown", () => {
+    const props = {
+      class: CLASS,
+      humberger: humberger,
+      flag: flg,
+    };
+
+    closeMenuByEscapeKey(props);
+  });
+
+  accordionTrigger.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      const props = {
+        event: e,
+        CLASS: CLASS,
+        accordionFlg: accordionFlg,
+      };
+
+      toggleShowAccordionInMenu(props);
+    });
+  });
+
+  // フォーカストラップ制御
+  focusTrap.addEventListener("focus", (e) => {
+    humberger.focus();
+  });
 });
